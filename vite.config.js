37cc77ca8af2f369a5b7f557cbf9917214f70a30
ISD <information.systemdatabase@gmail.com>
2026-09-04 12:33:37 +0900

    fix(vite): add allowedHosts to server and preview configuration

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,11 @@ export default defineConfig({
   },
   server: {
     port: 3000,
-    open: true
+    open: true,
+    allowedHosts: true
+  },
+  preview: {
+    port: 3000,
+    allowedHosts: true
   }
 })
